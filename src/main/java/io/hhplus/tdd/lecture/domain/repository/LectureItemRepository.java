da02e2d9574e6dac9b68f957eb31500ee6d3fadf
seungwon <swlee173@gmail.com>
2024-10-04 03:06:53 +0900

    style: 사용하지 않는 import문 제거

--- src/main/java/io/hhplus/tdd/lecture/domain/repository/LectureItemRepository.java
+++ src/main/java/io/hhplus/tdd/lecture/domain/repository/LectureItemRepository.java
@@ -1,11 +1,9 @@
 package io.hhplus.tdd.lecture.domain.repository;
 
 import io.hhplus.tdd.lecture.domain.model.LectureItemDTO;
-import io.hhplus.tdd.lecture.infrastructure.entity.LectureItem;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 public interface LectureItemRepository {
 
