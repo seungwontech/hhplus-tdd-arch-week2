ac88f75bf6f2c27183ff37729b4c50933c3769ad
seungwon <swlee173@gmail.com>
2024-10-04 09:56:16 +0900

    test: 동일한 유저 정보로 같은 특강을 5번 신청했을 때, 1번만 성공하는 것을 검증하는 통합 테스트 작성

--- src/main/java/io/hhplus/tdd/lecture/infrastructure/repository/LectureApplicationJpaRepository.java
+++ src/main/java/io/hhplus/tdd/lecture/infrastructure/repository/LectureApplicationJpaRepository.java
@@ -1,13 +1,17 @@
 package io.hhplus.tdd.lecture.infrastructure.repository;
 
 import io.hhplus.tdd.lecture.infrastructure.entity.LectureApplication;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 
 public interface LectureApplicationJpaRepository extends JpaRepository<LectureApplication, Long> {
-
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT li FROM LectureApplication li WHERE li.lectureItemId = :lectureItemId AND li.userId = :userId")
     LectureApplication findByLectureItemIdAndUserId(Long lectureItemId, Long userId);
 
     List<LectureApplication> findByUserId(Long userId);
